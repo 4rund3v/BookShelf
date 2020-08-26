b76f56ea60e9104504b5e895a412bc138e7d6c8f
arun dev p <4rund3v@gmail.com>
2020-08-26 22:58:52 +0530

    [Ui] Basic Structure of book table components completed.

---
+++ ui/src/components/sampleComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SampleComponent extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default SampleComponent;
