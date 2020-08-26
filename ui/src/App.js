21941589e061bdbffe5a4b613d1238c7c3285faf
arun dev p <4rund3v@gmail.com>
2020-08-26 22:58:52 +0530

    [Ui] Basic Structure of book table components completed.

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,11 +3,14 @@ import BookTable from './components/bookTable';
 
 class App extends Component {
     state = {  }
-    render() { 
-        return ( <div className='App'>Hello World
+    render() {        
+        return ( <div className='App'>Hello World 
         <BookTable />
         </div> );
     }
 }
- 
+
+//const App = appState => (<div className="App"> <h1>Hello World</h1> </div>);
+
 export default App;
+
