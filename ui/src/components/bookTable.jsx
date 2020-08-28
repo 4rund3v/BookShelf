555d8ed39f39cb9149e87ff8a3d2b220c16652dd
arun dev p <4rund3v@gmail.com>
2020-08-28 23:28:28 +0530

    [RB]React Table bootstarp for the table creation

--- ui/src/components/bookTable.jsx
+++ ui/src/components/bookTable.jsx
@@ -2,23 +2,61 @@ import React, { Component } from 'react';
 // named import {} & the default import 
 import { bookListing } from '../services/books_listing'
 
-
+//import Navbar from 'react-bootstrap/Navbar';
+import {Navbar, Nav, NavDropdown,FormControl,Form,Table, Button} from 'react-bootstrap';
+ 
 class BookTable extends Component {
-    state = {  }
-    
-    render() {
-        console.log(bookListing);
-        const localbookListing = bookListing;
+    state = { booklisting: [], notification: 0, footer:0 }
 
+    componentDidMount(){
+        console.log("Component did mount")
+        this.setState({ booklisting: bookListing})
+        //this.state = { booklisting: bookListing }
+    }
+
+    render() {
+        //console.log(bookListing);
+        // const localbookListing = bookListing;
+//         const navBar =  <Navbar bg="light" expand="lg">
+//                           <Navbar.Brand href="#home">BookShelf</Navbar.Brand>
+//                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                             <Navbar.Collapse id="basic-navbar-nav">
+//                           <Nav className="mr-auto">
+//       <Nav.Link href="#home">Home</Nav.Link>
+//       <Nav.Link href="#link">Link</Nav.Link>
+//       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//     <Form inline>
+//       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+//       <Button variant="outline-success">Search</Button>
+//     </Form>
+//   </Navbar.Collapse>
+// </Navbar>;
+
+
+
+
+
+        const tableStyle = { "border": "1px solid black" ,"padding": "10px"}
         const colNames = ["Book Title", "Author", "Year", "Series", "Rating"];
-        const tableHeader = <tr>{colNames.map((col) => <th>{col}</th>)} </tr>;
-        const tableBody = localbookListing.map((book) => <tr>
-                         <td>{book.title}</td>
-                         <td>{book.author}</td> 
-                         <td>{book.year}</td> 
-                         <td>{book.series}</td> </tr>);
-        const bookTable = <table className="bookTable">  {tableHeader} {tableBody} </table>;
-    return ( <div>{bookTable}</div>);
+        const tableHeader = <tr style={tableStyle}>{colNames.map((col) => <th>{col}</th>)} </tr>;
+
+        const tableBody = this.state.booklisting.map((book) => <tr key={book.id} style={tableStyle}>
+                         <td style={tableStyle}>{book.title}</td>
+                         <td style={tableStyle}>{book.author}</td> 
+                         <td style={tableStyle}>{book.year}</td> 
+                         <td style={tableStyle}>{book.series}</td>
+                         <td style={tableStyle}>{book.rating}</td>
+                          </tr>);
+        //const bootStrapTable = <Table striped bordered hover variant="dark">  <thead>   {tableHeader}   </thead>  {tableBody}     </Table>;
+        const bookTable = <table className="bookTable" style={tableStyle}>  {tableHeader} {tableBody} </table>;
+    return ( <div> {bookTable}</div>);
     }
 }
  
