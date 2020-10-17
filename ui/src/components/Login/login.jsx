10198092bbadc7f7bc725fb6d32726d4163239d8
arun dev <4rund3v@gmail.com>
2020-10-17 21:32:15 +0530

    [React] Added the login page for applicaiton.

--- ui/src/components/Login/login.jsx
+++ ui/src/components/Login/login.jsx
@@ -1,30 +1,45 @@
 import React, { Component } from "react";
-import { Form, Button } from "react-bootstrap";
-const Login = () => {
-  const data = (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+import { Form, Button, Row, Col } from "react-bootstrap";
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+class Login extends Component {
+  state = {};
 
-  return { data };
-};
+  prepareLoginPage() {
+    const loginForm = (
+      <div class="px-2">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Keep Me Logged in" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+    return loginForm;
+  }
+
+  render() {
+    const loginForm = this.prepareLoginPage();
+    return (
+      <Row className="loginForm">
+        <Col className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto  text-center form p-4">
+          <h1 class="display-4 py-2 text-truncate">Login to BookShelf.</h1>
+          {loginForm}
+        </Col>
+      </Row>
+    );
+  }
+}
 
 export default Login;
